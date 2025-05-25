fc99d4766c3ffe4f6e11b498007877347691bfb1
Alisher.Temirov <Alisher.Temirov@erg.kz>
2025-05-25 20:30:15 +0300

    check

--- users/views.py
+++ users/views.py
@@ -16,7 +16,8 @@ class CreateUpdateUserView(APIView):
     def post(self, request):
         data = request.data
         print("data: ", data)
-
+        print("check")
+        
         user_id = get_random_string(12)
         
         height = data.get("height", 175)
